e6c496401d20855835a68681aed937b4ad822e63
lushisi <lushisi258@qq.com>
2024-09-29 02:25:29 +0800

    lab1中断处理实现

--- lab1/kern/trap/trap.c
+++ lab1/kern/trap/trap.c
@@ -6,12 +6,12 @@
 #include <memlayout.h>
 #include <mmu.h>
 #include <riscv.h>
+#include <sbi.h>
 #include <stdio.h>
 #include <trap.h>
-#include <sbi.h>
 
 #define TICK_NUM 100
-volatile size_t num=0;
+volatile size_t num = 0;
 
 static void print_ticks() {
     cprintf("%d ticks\n", TICK_NUM);
@@ -85,99 +85,115 @@ void print_regs(struct pushregs *gpr) {
 void interrupt_handler(struct trapframe *tf) {
     intptr_t cause = (tf->cause << 1) >> 1;
     switch (cause) {
-        case IRQ_U_SOFT:
-            cprintf("User software interrupt\n");
-            break;
-        case IRQ_S_SOFT:
-            cprintf("Supervisor software interrupt\n");
-            break;
-        case IRQ_H_SOFT:
-            cprintf("Hypervisor software interrupt\n");
-            break;
-        case IRQ_M_SOFT:
-            cprintf("Machine software interrupt\n");
-            break;
-        case IRQ_U_TIMER:
-            cprintf("User software interrupt\n");
-            break;
-        case IRQ_S_TIMER:
-            // "All bits besides SSIP and USIP in the sip register are
-            // read-only." -- privileged spec1.9.1, 4.1.4, p59
-            // In fact, Call sbi_set_timer will clear STIP, or you can clear it
-            // directly.
-            // cprintf("Supervisor timer interrupt\n");
-             /* LAB1 EXERCISE2   YOUR CODE :  */
-            /*(1)设置下次时钟中断- clock_set_next_event()
-             *(2)计数器（ticks）加一
-             *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
-            * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
-            */
-            break;
-        case IRQ_H_TIMER:
-            cprintf("Hypervisor software interrupt\n");
-            break;
-        case IRQ_M_TIMER:
-            cprintf("Machine software interrupt\n");
-            break;
-        case IRQ_U_EXT:
-            cprintf("User software interrupt\n");
-            break;
-        case IRQ_S_EXT:
-            cprintf("Supervisor external interrupt\n");
-            break;
-        case IRQ_H_EXT:
-            cprintf("Hypervisor software interrupt\n");
-            break;
-        case IRQ_M_EXT:
-            cprintf("Machine software interrupt\n");
-            break;
-        default:
-            print_trapframe(tf);
-            break;
+    case IRQ_U_SOFT:
+        cprintf("User software interrupt\n");
+        break;
+    case IRQ_S_SOFT:
+        cprintf("Supervisor software interrupt\n");
+        break;
+    case IRQ_H_SOFT:
+        cprintf("Hypervisor software interrupt\n");
+        break;
+    case IRQ_M_SOFT:
+        cprintf("Machine software interrupt\n");
+        break;
+    case IRQ_U_TIMER:
+        cprintf("User software interrupt\n");
+        break;
+    case IRQ_S_TIMER:
+        // "All bits besides SSIP and USIP in the sip register are
+        // read-only." -- privileged spec1.9.1, 4.1.4, p59
+        // In fact, Call sbi_set_timer will clear STIP, or you can clear it
+        // directly.
+        // cprintf("Supervisor timer interrupt\n");
+        /* LAB1 EXERCISE2   YOUR CODE : 2213601 */
+        /*(1)设置下次时钟中断- clock_set_next_event()
+         *(2)计数器（ticks）加一
+         *(3)当计数器加到100的时候，我们会输出一个`100ticks`表示我们触发了100次时钟中断，同时打印次数（num）加一
+         * (4)判断打印次数，当打印次数为10时，调用<sbi.h>中的关机函数关机
+         */
+
+        // LAB1 YOUR CODE:2213601
+        // 设置下次时钟中断
+        clock_set_next_event();
+        // 计数器ticks加一
+        ticks++;
+        // 当计数器ticks加到100时，输出消息并增加num计数器
+        if (ticks == 100) {
+            print_ticks();
+            ticks = 0; // 重置ticks计数器
+            num++;
+        }
+        // 判断打印次数num，当num达到10时，调用关机函数
+        if (num == 10) {
+            sbi_shutdown();
+        }
+        break;
+    case IRQ_H_TIMER:
+        cprintf("Hypervisor software interrupt\n");
+        break;
+    case IRQ_M_TIMER:
+        cprintf("Machine software interrupt\n");
+        break;
+    case IRQ_U_EXT:
+        cprintf("User software interrupt\n");
+        break;
+    case IRQ_S_EXT:
+        cprintf("Supervisor external interrupt\n");
+        break;
+    case IRQ_H_EXT:
+        cprintf("Hypervisor software interrupt\n");
+        break;
+    case IRQ_M_EXT:
+        cprintf("Machine software interrupt\n");
+        break;
+    default:
+        print_trapframe(tf);
+        break;
     }
 }
 
 void exception_handler(struct trapframe *tf) {
     switch (tf->cause) {
-        case CAUSE_MISALIGNED_FETCH:
-            break;
-        case CAUSE_FAULT_FETCH:
-            break;
-        case CAUSE_ILLEGAL_INSTRUCTION:
-             // 非法指令异常处理
-             /* LAB1 CHALLENGE3   YOUR CODE :  */
-            /*(1)输出指令异常类型（ Illegal instruction）
-             *(2)输出异常指令地址
-             *(3)更新 tf->epc寄存器
-            */
-            break;
-        case CAUSE_BREAKPOINT:
-            //断点异常处理
-            /* LAB1 CHALLLENGE3   YOUR CODE :  */
-            /*(1)输出指令异常类型（ breakpoint）
-             *(2)输出异常指令地址
-             *(3)更新 tf->epc寄存器
-            */
-            break;
-        case CAUSE_MISALIGNED_LOAD:
-            break;
-        case CAUSE_FAULT_LOAD:
-            break;
-        case CAUSE_MISALIGNED_STORE:
-            break;
-        case CAUSE_FAULT_STORE:
-            break;
-        case CAUSE_USER_ECALL:
-            break;
-        case CAUSE_SUPERVISOR_ECALL:
-            break;
-        case CAUSE_HYPERVISOR_ECALL:
-            break;
-        case CAUSE_MACHINE_ECALL:
-            break;
-        default:
-            print_trapframe(tf);
-            break;
+    case CAUSE_MISALIGNED_FETCH:
+        break;
+    case CAUSE_FAULT_FETCH:
+        break;
+    case CAUSE_ILLEGAL_INSTRUCTION:
+        // 非法指令异常处理
+        /* LAB1 CHALLENGE3   YOUR CODE :  */
+        /*(1)输出指令异常类型（ Illegal instruction）
+         *(2)输出异常指令地址
+         *(3)更新 tf->epc寄存器
+         */
+        break;
+    case CAUSE_BREAKPOINT:
+        // 断点异常处理
+        /* LAB1 CHALLLENGE3   YOUR CODE :  */
+        /*(1)输出指令异常类型（ breakpoint）
+         *(2)输出异常指令地址
+         *(3)更新 tf->epc寄存器
+         */
+        break;
+    case CAUSE_MISALIGNED_LOAD:
+        break;
+    case CAUSE_FAULT_LOAD:
+        break;
+    case CAUSE_MISALIGNED_STORE:
+        break;
+    case CAUSE_FAULT_STORE:
+        break;
+    case CAUSE_USER_ECALL:
+        break;
+    case CAUSE_SUPERVISOR_ECALL:
+        break;
+    case CAUSE_HYPERVISOR_ECALL:
+        break;
+    case CAUSE_MACHINE_ECALL:
+        break;
+    default:
+        print_trapframe(tf);
+        break;
     }
 }
 
